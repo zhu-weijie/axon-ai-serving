1568a617f3251f58f3f32abc91ff41bb87411ae2
zhuwj <zhuwj@asis-technologies.com>
2024-09-04 10:24:51 +0800

    Added rename_files and changed manage.py to uwsgi

---
+++ README.md
@@ -0,0 +1,8 @@
+## Version v1.1.0
+
+- Added the ability to allow the upgrade file names to contain version numbers and other information, provided that the upgrade file name must include either "p900master" or "p900webserver" to be valid for the upgrade process.
+- Changed `manage.py` to `uwsgi` to identify the p900webserver pid.
+
+## Version v1.0.0
+
+- Added basic functionality to the program.
--- p900upgrade.py
+++ p900upgrade.py
@@ -3,107 +3,107 @@ import time
 import zipfile
 from threading import Timer
 
-VER = "1.0"
+VERSION = "1.1.0"
 
-FILE_UpgradeFlag       = "/root/upload/UpgradePending"
+FILE_UPGRADE_FLAG = "/root/upload/upgrade_pending"
 
-FILE_p900master        = "/root/upload/p900master.zip"
-FILE_p900masterMD5     = "/root/upload/p900master.zip.md5"
+FILE_P900MASTER = "/root/upload/p900master.zip"
+FILE_P900MASTER_MD5 = "/root/upload/p900master.zip.md5"
 
-FILE_p900webserver     = "/root/upload/p900webserver.zip"
-FILE_p900webserverMD5  = "/root/upload/p900webserver.zip.md5"
-
-
-#def checkMD5(file, fileMD5):
-#    md5 = os.popen("md5sum -b " + file).readline()
-#    fd = open(fileMD5)
-#    md5ref = fd.readline()
-#    fd.close()
-#    if ( operator.eq(md5[0:31], md5ref[0:31]) ):
-#        return True
-#    else:
-#        return False
+FILE_P900WEBSERVER = "/root/upload/p900webserver.zip"
+FILE_P900WEBSERVER_MD5 = "/root/upload/p900webserver.zip.md5"
 
 
-def p900upgrade_Master():
-    if ( os.path.isfile(FILE_p900master) != True or os.path.isfile(FILE_p900masterMD5) != True ):
+def rename_files():
+    for file in os.listdir("/root/upload"):
+        if "p900master" in file:
+            if file.endswith(".zip"):
+                os.rename(os.path.join("/root/upload", file), FILE_P900MASTER)
+            elif file.endswith(".md5"):
+                os.rename(os.path.join("/root/upload", file), FILE_P900MASTER_MD5)
+        elif "p900webserver" in file:
+            if file.endswith(".zip"):
+                os.rename(os.path.join("/root/upload", file), FILE_P900WEBSERVER)
+            elif file.endswith(".md5"):
+                os.rename(os.path.join("/root/upload", file), FILE_P900WEBSERVER_MD5)
+
+
+def p900upgrade_master():
+    if not os.path.isfile(FILE_P900MASTER) or not os.path.isfile(FILE_P900MASTER_MD5):
         return False
-    else:#if ( checkMD5(FILE_p900master, FILE_p900masterMD5) ):
-        print("delete old p900master")
-        os.system("rm -f /root/p900master")
-        os.system("sync")
-        os.system("sleep 1")
 
-        print("decompress new p900master")
-        os.system("unzip -q -o -d /root " + FILE_p900master)
-        os.system("sync")
-        os.system("sleep 1")
+    print("delete old p900master")
+    os.system("rm -f /root/p900master")
+    os.system("sync")
+    time.sleep(1)
 
-        os.system("chmod 775 /root/p900master")
-        os.system("sync")
-        os.system("sleep 1")
+    print("decompress new p900master")
+    os.system(f"unzip -q -o -d /root {FILE_P900MASTER}")
+    os.system("sync")
+    time.sleep(1)
 
-        return True
-#    else:
-#        print("p900upgrade Master fail: md5 not match")
-#        return False
+    os.system("chmod 775 /root/p900master")
+    os.system("sync")
+    time.sleep(1)
+
+    return True
 
 
-def p900upgrade_Webserver():
-    if ( os.path.isfile(FILE_p900webserver) != True or os.path.isfile(FILE_p900webserverMD5) != True ):
+def p900upgrade_webserver():
+    if not os.path.isfile(FILE_P900WEBSERVER) or not os.path.isfile(
+        FILE_P900WEBSERVER_MD5
+    ):
         return False
-    else:#if ( checkMD5(FILE_p900webserver, FILE_p900webserverMD5) ):
-        print("stop old p900webserver")
-        while(1):
-            pid = os.popen("ps -eaf | grep manage.py | grep -v grep").readline()
-            if (pid != ""):
-                pid = pid[0:5]
-                print("kill pid " + pid)
-                os.system("kill " + pid)
-                os.system("sync")
-                time.sleep(0.1)
-            else:
-                break
-        
-        print("create backup for setting folders")
-        os.system("mkdir -p /root/tmpData/{floorplans,script}")
-        os.system("cp -avr /root/p900webserver/media/floorplans/* /root/tmpData/floorplans")
-        os.system("cp -avr /root/p900webserver/script/import/* /root/tmpData/script")
-        
-        print("delete old p900webserver")
-        os.system("rm -rf /root/p900webserver")
-        os.system("sync")
-        time.sleep(1)
-        
-        print("decompress new p900webserver")
-        os.system("unzip -q -o -d /root " + FILE_p900webserver)
-        print('success extract zip file')
-        os.system("sync")
-        time.sleep(1)
-        
-        print("restore setting folders")
-        os.system("cp -avr /root/tmpData/floorplans/* /root/p900webserver/media/floorplans")
-        os.system("cp -avr /root/tmpData/script/* /root/p900webserver/script/import")
-        os.system("sync")
-        time.sleep(1)
-        
-        print("delete backup folder")
-        os.system("rm -rf /root/tmpData")
-        os.system("sync")
-        time.sleep(1)
-        
-        return True
-#    else:
-#        print("p900upgrade Webserver fail: md5 not match")
-#        return False
+
+    print("stop old p900webserver")
+    while True:
+        pid = os.popen("ps -eaf | grep uwsgi | grep -v grep").readline()
+        if pid:
+            print(f"kill pid {pid}")
+            os.system(f"kill {pid}")
+            os.system("sync")
+            time.sleep(0.1)
+        else:
+            break
+
+    print("create backup for setting folders")
+    os.system("mkdir -p /root/tmpData/{floorplans,script}")
+    os.system("cp -avr /root/p900webserver/media/floorplans/* /root/tmpData/floorplans")
+    os.system("cp -avr /root/p900webserver/script/import/* /root/tmpData/script")
+
+    print("delete old p900webserver")
+    os.system("rm -rf /root/p900webserver")
+    os.system("sync")
+    time.sleep(1)
+
+    print("decompress new p900webserver")
+    os.system(f"unzip -q -o -d /root {FILE_P900WEBSERVER}")
+    print("success extract zip file")
+    os.system("sync")
+    time.sleep(1)
+
+    print("restore setting folders")
+    os.system("cp -avr /root/tmpData/floorplans/* /root/p900webserver/media/floorplans")
+    os.system("cp -avr /root/tmpData/script/* /root/p900webserver/script/import")
+    os.system("sync")
+    time.sleep(1)
+
+    print("delete backup folder")
+    os.system("rm -rf /root/tmpData")
+    os.system("sync")
+    time.sleep(1)
+
+    return True
 
 
-def p900upgrade(inc):
-    #print('P900Upgrade')
-    if ( os.path.isfile(FILE_UpgradeFlag) == True ):
-        print("p900upgrade: start upgrade")
-        rt1 = p900upgrade_Webserver()
-        rt2 = p900upgrade_Master()
+def p900upgrade(interval):
+    if os.path.isfile(FILE_UPGRADE_FLAG):
+        print("rename upload files...")
+        rename_files()
+
+        print("p900upgrade: start upgrade...")
+        rt1 = p900upgrade_webserver()
+        rt2 = p900upgrade_master()
 
         print("p900upgrade: delete upload files...")
         os.system("rm -rf /root/upload/*")
@@ -111,16 +111,14 @@ def p900upgrade(inc):
         print("p900upgrade: finish upgrade")
         time.sleep(1)
 
-        if (rt1 == True or rt2 == True):
+        if rt1 or rt2:
             print("p900upgrade: system reboot...")
             os.system("reboot")
             time.sleep(60)
 
-    t = Timer(inc, p900upgrade, (inc,))
+    t = Timer(interval, p900upgrade, (interval,))
     t.start()
 
 
-print("\np900upgrade: version " + VER)
+print(f"\np900upgrade: version {VERSION}")
 p900upgrade(3)
-
-
