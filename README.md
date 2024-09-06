9d279b7b210c2d69cd3d0dd08bae4c0328399142
zhuwj <zhuwj@asis-technologies.com>
2024-09-06 15:25:00 +0800

    Removed debugging code

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Version v1.1.0
 
 - Added the ability to allow the upgrade file names to contain version numbers and other information, provided that the upgrade file name must include either "p900master" or "p900webserver" to be valid for the upgrade process.
-- Changed `manage.py` to `uwsgi` to identify the p900webserver pid.
+- Removed killing pid steps before replacing the upgrade file.
 
 ## Version v1.0.0
 
--- p900upgrade.py
+++ p900upgrade.py
@@ -29,16 +29,17 @@ def rename_files():
 
 
 def p900upgrade_master():
-    if not os.path.isfile(FILE_P900MASTER) or not os.path.isfile(FILE_P900MASTER_MD5):
+    if not os.path.isfile(FILE_P900MASTER):
+        return False
+    if not os.path.isfile(FILE_P900MASTER_MD5):
         return False
 
-    print("delete old p900master")
     os.system("rm -f /root/p900master")
     os.system("sync")
     time.sleep(1)
 
-    print("decompress new p900master")
-    os.system(f"unzip -q -o -d /root {FILE_P900MASTER}")
+    if os.system(f"unzip -q -o -d /root {FILE_P900MASTER}") != 0:
+        return False
     os.system("sync")
     time.sleep(1)
 
@@ -50,46 +51,30 @@ def p900upgrade_master():
 
 
 def p900upgrade_webserver():
-    if not os.path.isfile(FILE_P900WEBSERVER) or not os.path.isfile(
-        FILE_P900WEBSERVER_MD5
-    ):
+    if not os.path.isfile(FILE_P900WEBSERVER):
+        return False
+    if not os.path.isfile(FILE_P900WEBSERVER_MD5):
         return False
 
-    print("stop old p900webserver")
-    while True:
-        pid = os.popen("ps -eaf | grep uwsgi | grep -v grep").readline()
-        if pid:
-            print(f"kill pid {pid}")
-            os.system(f"kill {pid}")
-            os.system("sync")
-            time.sleep(0.1)
-        else:
-            break
+    os.system("mkdir -p /root/tmp_data/{floorplans,script}")
+    os.system("cp -avr /root/p900webserver/media/floorplans/* /root/tmp_data/floorplans")
+    os.system("cp -avr /root/p900webserver/script/import/* /root/tmp_data/script")
 
-    print("create backup for setting folders")
-    os.system("mkdir -p /root/tmpData/{floorplans,script}")
-    os.system("cp -avr /root/p900webserver/media/floorplans/* /root/tmpData/floorplans")
-    os.system("cp -avr /root/p900webserver/script/import/* /root/tmpData/script")
-
-    print("delete old p900webserver")
     os.system("rm -rf /root/p900webserver")
     os.system("sync")
     time.sleep(1)
 
-    print("decompress new p900webserver")
-    os.system(f"unzip -q -o -d /root {FILE_P900WEBSERVER}")
-    print("success extract zip file")
+    if os.system(f"unzip -q -o -d /root {FILE_P900WEBSERVER}") != 0:
+        return False
     os.system("sync")
     time.sleep(1)
 
-    print("restore setting folders")
-    os.system("cp -avr /root/tmpData/floorplans/* /root/p900webserver/media/floorplans")
-    os.system("cp -avr /root/tmpData/script/* /root/p900webserver/script/import")
+    os.system("cp -avr /root/tmp_data/floorplans/* /root/p900webserver/media/floorplans")
+    os.system("cp -avr /root/tmp_data/script/* /root/p900webserver/script/import")
     os.system("sync")
     time.sleep(1)
 
-    print("delete backup folder")
-    os.system("rm -rf /root/tmpData")
+    os.system("rm -rf /root/tmp_data")
     os.system("sync")
     time.sleep(1)
 
@@ -98,27 +83,22 @@ def p900upgrade_webserver():
 
 def p900upgrade(interval):
     if os.path.isfile(FILE_UPGRADE_FLAG):
-        print("rename upload files...")
         rename_files()
 
-        print("p900upgrade: start upgrade...")
         rt1 = p900upgrade_webserver()
         rt2 = p900upgrade_master()
 
-        print("p900upgrade: delete upload files...")
         os.system("rm -rf /root/upload/*")
         os.system("sync")
-        print("p900upgrade: finish upgrade")
-        time.sleep(1)
 
         if rt1 or rt2:
-            print("p900upgrade: system reboot...")
             os.system("reboot")
             time.sleep(60)
+        else:
+            print("Upgrade failed. No reboot will be performed.")
 
     t = Timer(interval, p900upgrade, (interval,))
     t.start()
 
 
-print(f"\np900upgrade: version {VERSION}")
 p900upgrade(3)
